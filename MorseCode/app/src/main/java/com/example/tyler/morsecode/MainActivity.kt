7245c4f9c823fac2f11728f3801f040d29c2f63a
tylsarch <tylsarch@ut.utm.edu>
2018-02-11 21:17:05 -0600

    Part 4 and 5
    forgot to stagger commits

--- MorseCode/app/src/main/java/com/example/tyler/morsecode/MainActivity.kt
+++ MorseCode/app/src/main/java/com/example/tyler/morsecode/MainActivity.kt
@@ -3,7 +3,15 @@ package com.example.tyler.morsecode
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.media.AudioAttributes
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioManager.STREAM_MUSIC
+import android.media.AudioTrack
 import android.os.Bundle
+import android.preference.PreferenceManager.getDefaultSharedPreferences
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.text.method.ScrollingMovementMethod
@@ -16,17 +24,30 @@ import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import org.json.JSONObject
+import java.lang.Math.round
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
 
     var letToCodeDict: HashMap<String, String> = HashMap();
     var codeToLetDict: HashMap<String, String> = HashMap();
+    val dotLength: Int = 50
+    val dashLength: Int = dotLength * 3
+    var dotSoundBuffer: ShortArray = genSineWaveSoundBuffer(550.0, dotLength)
+    var dashSoundBuffer: ShortArray = genSineWaveSoundBuffer(550.0, dashLength)
+
+    var prefs: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         mtextView.movementMethod = ScrollingMovementMethod()
+
+        prefs = getDefaultSharedPreferences(this.applicationContext)
+
+        val morsePitch = prefs!!.getString("morse_pitch", "550").toInt()
 
         buildDictsWithJSON(loadMorseJSON())
 
@@ -37,6 +58,8 @@ class MainActivity : AppCompatActivity() {
         codesButton.setOnClickListener{showCodes(); hideKeyboard();}
 
         translateButton.setOnClickListener{translate(); hideKeyboard();}
+
+        playButton.setOnClickListener{playString(mtextView.text.toString()); hideKeyboard();}
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -162,6 +185,121 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun playString(s:String, i: Int = 0): Unit
+    {
+        if(i > s.length -  1)
+        {
+            return
+        }
+
+        var mDelay: Long = 0
+
+        var thenFun: () -> Unit = { -> this@MainActivity.runOnUiThread(java.lang.Runnable { playString(s, i+1) })}
+
+        var c = s[i]
+
+        Log.d("Log", "Processing pos: " + i + " char : [" + c + "]")
+        if(c == '.')
+        {
+            playDot(thenFun)
+        }
+        else if(c == '-')
+        {
+            playDash(thenFun)
+        }
+        else if(c == '/')
+        {
+            pause(6*dotLength, thenFun)
+        }
+        else if(c == ' ')
+        {
+            pause(2*dotLength, thenFun)
+        }
+    }
+
+    fun playDash(onDone : () -> Unit = {})
+    {
+        Log.d("DEBUG", "playDash")
+        playSoundBuffer(dashSoundBuffer, { -> pause(dotLength,onDone)})
+    }
+
+    fun playDot(onDone : () -> Unit = {})
+    {
+        Log.d("DEBUG", "playDot")
+        playSoundBuffer(dotSoundBuffer, { -> pause(dotLength,onDone)})
+    }
+
+    fun pause(durationMSec: Int, onDone : () -> Unit = {})
+    {
+        Log.d("DEBUG", "pause: " + durationMSec)
+        Timer().schedule(timerTask { onDone() }, durationMSec.toLong())
+    }
+
+    val SAMPLE_RATE = 44100
+
+    private fun genSineWaveSoundBuffer(frequency: Double, durationMSec: Int) : ShortArray
+    {
+        val duration: Int = round((durationMSec / 1000.0) * SAMPLE_RATE).toInt()
+
+        var mSound: Double
+        var mBuffer = ShortArray(duration)
+
+        for(i in 0 until duration)
+        {
+            mSound = Math.sin(2.0 * Math.PI * i.toDouble() / (SAMPLE_RATE / frequency))
+            mBuffer[i] = (mSound * java.lang.Short.MAX_VALUE).toShort()
+        }
+
+        return mBuffer
+
+    }
+
+    private fun playSoundBuffer(mBuffer: ShortArray, onDone : () -> Unit = {})
+    {
+        var minBufferSize = SAMPLE_RATE / 10
+        if(minBufferSize < mBuffer.size)
+        {
+            minBufferSize = minBufferSize + minBufferSize * (Math.round(mBuffer.size.toFloat()) / minBufferSize.toFloat()).toInt()
+        }
+
+
+        val nBuffer = ShortArray(minBufferSize)
+        for(i in nBuffer.indices)
+        {
+            if(i < mBuffer.size)
+            {
+                nBuffer[i] = mBuffer[i]
+            }
+            else
+            {
+                nBuffer[i] = 0
+            }
+        }
+
+        val mAudioTrack = AudioTrack(STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, minBufferSize, AudioTrack.MODE_STREAM)
+
+        mAudioTrack.setStereoVolume(AudioTrack.getMaxVolume(), AudioTrack.getMaxVolume())
+        mAudioTrack.setNotificationMarkerPosition(mBuffer.size)
+        mAudioTrack.setPlaybackPositionUpdateListener(object: AudioTrack.OnPlaybackPositionUpdateListener
+        {
+            override fun onPeriodicNotification(track: AudioTrack)
+            {
+
+            }
+
+            override fun onMarkerReached(track: AudioTrack) {
+                Log.d("Log", "Audio track end of file reached.")
+                mAudioTrack.stop()
+                mAudioTrack.release()
+                onDone()
+            }
+        })
+
+        mAudioTrack.play()
+        mAudioTrack.write(nBuffer, 0, minBufferSize)
+
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -173,7 +311,12 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
